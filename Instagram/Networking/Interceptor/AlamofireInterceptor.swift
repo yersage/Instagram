68a3d9a0517136134c7d85f49029eea2b044c4b0
Yersage <yersage@gmail.com>
2022-01-24 14:25:30 +0600

    Create new abstraction

---
+++ Instagram/Networking/Interceptor/AlamofireInterceptor.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireInterceptor.swift
+//  Instagram
+//
+//  Created by Yersage on 24.01.2022.
+//
+
+import Foundation
+import Alamofire
+
+protocol AlamofireInterceptor {
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void)
+    func retry(_ request: Request, for session: Session, dueTo error: Error,
+               completion: @escaping (RetryResult) -> Void)
+}
