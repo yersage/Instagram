78547d38f851087d20407b8f6eb640af339984f2
Yersage <yersage@gmail.com>
2022-01-27 14:48:51 +0600

    USe router for vc navigation

--- Instagram/Pages/Sign In/SignInViewController.swift
+++ Instagram/Pages/Sign In/SignInViewController.swift
@@ -16,10 +16,12 @@ final class SignInViewController: UIViewController, SignInViewDelegate {
     @IBOutlet weak var loginButton: UIButton!
     
     private var presenter: SignInPresenter?
+    private var router = SignInRouter()
     // как отдать навКонтроллер пресентеру?
     
     // MARK:- Lifecycle functions
     override func viewDidLoad() {
+        router.viewController = self
         presenter = SignInPresenter()
         presenter?.view = self
 
@@ -56,15 +58,7 @@ final class SignInViewController: UIViewController, SignInViewDelegate {
     }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
-        guard let emailVC = storyboard?.instantiateViewController(withIdentifier: "EmailViewController") as? EmailViewController else { return }
-        let presenter = EmailPresenter()
-        presenter.view = emailVC
-        emailVC.presenter = EmailPresenter()
-        
-        let backItem = UIBarButtonItem()
-        navigationItem.backBarButtonItem = backItem
-        
-        navigationController?.pushViewController(emailVC, animated: true)
+        router.navigate(.toEmailVC)
     }
     
     // MARK:- SignInViewDelegate funcs
@@ -75,46 +69,7 @@ final class SignInViewController: UIViewController, SignInViewDelegate {
     }
     
     func goToFeedVC() {
-        let tabBarVC = UITabBarController()
-        
-        guard let feedVC = storyboard?.instantiateViewController(withIdentifier: "FeedViewController") as? FeedViewController else { return }
-        let feedPresenter = FeedPresenter()
-        feedPresenter.view = feedVC
-        feedVC.presenter = feedPresenter
-        
-        guard let searchVC = storyboard?.instantiateViewController(withIdentifier: "SearchViewController") as? SearchViewController else { return }        
-        let searchPresenter = SearchPresenter()
-        searchPresenter.view = searchVC
-        searchVC.presenter = searchPresenter
-        
-        guard let createPostVC = storyboard?.instantiateViewController(withIdentifier: "CreatePostViewController") as? CreatePostViewController else { return }
-        let createPostPresenter = CreatePostPresenter()
-        createPostPresenter.view = createPostVC
-        createPostVC.presenter = createPostPresenter
-        
-        guard let notificationsVC = storyboard?.instantiateViewController(withIdentifier: "NotificationsViewController") as? NotificationsViewController else { return }
-        
-        guard let selfProfileVC = storyboard?.instantiateViewController(withIdentifier: "SelfProfileViewController") as? SelfProfileViewController else { return }
-        let selfProfilePresenter = SelfProfilePresenter()
-        selfProfilePresenter.view = selfProfileVC
-        selfProfileVC.presenter = selfProfilePresenter
-        
-        feedVC.title = "Feed"
-        searchVC.title = "Search"
-        createPostVC.title = "Create Post"
-        notificationsVC.title = "Notifications"
-        selfProfileVC.title = "Profile"
-            
-        tabBarVC.setViewControllers([
-            UINavigationController(rootViewController: feedVC),
-            UINavigationController(rootViewController: searchVC),
-            UINavigationController(rootViewController: createPostVC),
-            UINavigationController(rootViewController: notificationsVC),
-            UINavigationController(rootViewController: selfProfileVC)],
-                                    animated: false)
-        
-        tabBarVC.modalPresentationStyle = .fullScreen
-        navigationController?.pushViewController(tabBarVC, animated: false)
+        router.navigate(.toFeedVC)
     }
 }
 
