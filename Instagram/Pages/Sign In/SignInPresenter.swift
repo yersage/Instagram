b588c98a50bb560ee94d67f92001371d878d1464
Yersage <yersage@gmail.com>
2022-01-29 15:20:09 +0600

    Remove networkManager's usage

--- Instagram/Pages/Sign In/SignInPresenter.swift
+++ Instagram/Pages/Sign In/SignInPresenter.swift
@@ -10,7 +10,6 @@ import KeychainSwift
 
 final class SignInPresenter: SignInPresenterDelegate {
     weak var view: SignInViewDelegate?
-    private let networkManager: NetworkManager = NetworkManager()
     private let keychainService: KeychainServiceDelegate = KeychainSwiftService(decoder: JWTDecoder())
     private let authService = AuthService()
     
@@ -33,20 +32,5 @@ final class SignInPresenter: SignInPresenterDelegate {
                 self?.view?.show(error: error.localizedDescription)
             }
         }
-        
-        /*
-        networkManager.noInterceptorRequest(InstagramEndPoint.login(username: username, password: password)) { [self] (result: Result<TokenModel, Error>) -> Void in
-            switch result {
-            case .success(let tokenModel):
-                keychain.set(tokenModel.accessToken, forKey: K.keychainAccessTokenKey)
-                keychain.set(tokenModel.refreshToken, forKey: K.keychainRefreshTokenKey)
-                keychain.set(username, forKey: K.keychainUsernameKey)
-                keychain.set(password, forKey: K.keychainPasswordKey)
-                self.view?.goToFeedVC()
-            case .failure(let error):
-                self.view?.show(error: error.localizedDescription)
-            }
-        }
-         */
     }
 }
