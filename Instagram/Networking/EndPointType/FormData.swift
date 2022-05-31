b36bf72ccb653de19693454a9e3651134118ac31
Yersage <yersage@gmail.com>
2022-05-31 13:52:29 +0600

    Reorganize EndPointType

---
+++ Instagram/Networking/EndPointType/FormData.swift
@@ -0,0 +1,15 @@
+//
+//  FormData.swift
+//  Instagram
+//
+//  Created by Yersage on 31.05.2022.
+//
+
+import Foundation
+
+struct FormData {
+    var data: Data
+    var withName: String
+    var fileName: String?
+    var mimeType: String?
+}
